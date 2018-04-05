a0da34a5ce8baab7f692b64e5a0f8e4db8a52f31
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2018-04-05 13:58:52 -0700

    Ejercicio Base 1

--- src/exercise1/index.php
+++ src/exercise1/index.php
@@ -1,2 +1,34 @@
 <?php
-phpinfo();
+class example1{
+    protected $foo;
+
+    /**
+     * @return mixed
+     */
+    public function getFoo()
+    {
+        return $this->foo;
+    }
+
+    /**
+     * @param mixed $foo
+     */
+    public function setFoo($foo)
+    {
+        $this->foo = $foo;
+    }
+
+
+}
+function changeExample($param) {
+    $param->setFoo('blah');
+    return $param->getFoo();
+}
+
+$obj        = new example1();
+$obj->setFoo("bar");
+echo $obj->getFoo();
+echo "<br>\n";
+echo changeExample($obj);
+echo "<br>\n";
+echo $obj->getFoo();
